69f1ea8ab3d64ba4427653bf6adc46ddecd664f4
bstewart3 <505propz@gmail.com>
2023-01-30 17:36:34 -0800

    add game play logic to several components. functional but unstyled

---
+++ src/components/StoryLine.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p v-for="(storyLine, index) in currentStoryLine" :key="index">
+      {{ storyLine }}
+    </p>
+    <p>{{ gameIndex }}</p>
+  </div>
+</template>
+
+<script>
+import { useGame } from "../stores/useGame";
+
+export default {
+  setup() {
+    const { story, storyLines, advanceStory } = useGame();
+    const currentStoryLine = [story.storyLines[story.currentStoryIndex]];
+
+    function incNum(num) {
+      setInterval(() => {
+        num + 1;
+      }, 1000);
+      return console.log(num);
+    }
+    incNum(1);
+    // setInterval(() => {
+    //   advanceStory();
+    // }, 1000);
+
+    return { story, currentStoryLine, storyLines, advanceStory };
+  },
+  computed: {
+    gameIndex() {
+      console.log(this.story.currentStoryIndex);
+      return this.story.currentStoryIndex;
+    },
+  },
+};
+</script>
